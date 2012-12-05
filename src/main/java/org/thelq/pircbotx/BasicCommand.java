fa86b206fbf4218ed25d8a66f891ce1c46cf7552
Leon Blakey <lord.quackstar@gmail.com>
2012-12-05 17:50:15 -0500

    Moved Basic command back to project root since core doesn't make any sense

--- src/main/java/org/thelq/pircbotx/BasicCommand.java
+++ src/main/java/org/thelq/pircbotx/BasicCommand.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Quackbot. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.thelq.pircbotx.core;
+package org.thelq.pircbotx;
 
 /**
  *
