e437bff70736551fc9fa396ffe8fd154e51f38a2
Leon Blakey <lord.quackstar@gmail.com>
2012-12-05 22:18:41 -0500

    Forgot the license header

--- src/main/java/org/thelq/pircbotx/commands/UptimeCommand.java
+++ src/main/java/org/thelq/pircbotx/commands/UptimeCommand.java
@@ -1,7 +1,24 @@
+/**
+ * Copyright (C) 2011 Leon Blakey <lord.quackstar at gmail.com>
+ *
+ * This file is part of Quackbot.
+ *
+ * Quackbot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Quackbot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Quackbot. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.thelq.pircbotx.commands;
 
 import java.lang.management.ManagementFactory;
-import java.util.Date;
 import lombok.Getter;
 import org.apache.commons.lang3.time.DateUtils;
 import org.pircbotx.hooks.ListenerAdapter;
