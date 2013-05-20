2f569edd5c5fc9be46938c935e330d32d079ec8e
Leon Blakey <lord.quackstar@gmail.com>
2013-05-20 02:45:05 -0400

    tjws snapshot

---
+++ src/main/java/org/thelq/pircbotx/server/BotServe.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2011 Leon Blakey <lord.quackstar at gmail.com>
+ *
+ * This file is part of TheLQ-PircBotX.
+ *
+ * TheLQ-PircBotX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TheLQ-PircBotX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TheLQ-PircBotX. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.thelq.pircbotx.server;
+
+import Acme.Serve.Serve;
+import com.google.common.collect.ImmutableMap;
+import java.io.IOException;
+import java.util.Properties;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.velocity.tools.view.VelocityViewServlet;
+
+/**
+ *
+ * @author Leon
+ */
+@Slf4j
+public class BotServe extends Serve {
+	public BotServe(int port) {
+		super(ImmutableMap.builder()
+				.put(ARG_PORT, port)
+				.put(ARG_NOHUP, "nohup")
+				.put(ARG_WORK_DIRECTORY, "c:\\users")
+				.build(), System.out);
+
+		
+		addServlet("/", new VelocityViewServlet());
+		addServlet("/myServe", new HttpServlet() {
+			@Override
+			protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+				response.getWriter().write("Hello world!");
+			}
+		});
+	}
+}
