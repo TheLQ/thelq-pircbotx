5a8f9d233bbf29a52027239d3af4dd7b285507b8
Leon Blakey <lord.quackstar@gmail.com>
2013-05-20 08:34:20 -0400

    Use manager stat uptime in uptimecommand

--- src/main/java/org/thelq/pircbotx/commands/UptimeCommand.java
+++ src/main/java/org/thelq/pircbotx/commands/UptimeCommand.java
@@ -18,12 +18,11 @@
  */
 package org.thelq.pircbotx.commands;
 
-import java.lang.management.ManagementFactory;
 import lombok.Getter;
-import org.apache.commons.lang3.time.DateUtils;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
 import org.thelq.pircbotx.BasicCommand;
+import org.thelq.pircbotx.Main;
 
 /**
  *
@@ -37,22 +36,8 @@ public class UptimeCommand extends ListenerAdapter implements BasicCommand {
 	public void onMessage(MessageEvent event) throws Exception {
 		if (!event.getMessage().startsWith("?uptime"))
 			return;
-
-		//Get the uptime of the JVM
-		long uptime = ManagementFactory.getRuntimeMXBean().getUptime();
-
-		//Calculate pieces of the date
-		long remainingTime = uptime;
-		long days = remainingTime / DateUtils.MILLIS_PER_DAY;
-		remainingTime -= days * DateUtils.MILLIS_PER_DAY;
-		long hours = remainingTime / DateUtils.MILLIS_PER_HOUR;
-		remainingTime -= hours * DateUtils.MILLIS_PER_HOUR;
-		long mins = remainingTime / DateUtils.MILLIS_PER_MINUTE;
-		remainingTime -= mins * DateUtils.MILLIS_PER_MINUTE;
-		long secs = remainingTime / DateUtils.MILLIS_PER_SECOND;
 		
 		//Done, inform the user
-		event.respond("This bot has been up for " + days + " days, " + hours + " hours, " 
-				+ mins + " minuites, and " + secs + " seconds");
+		event.respond("This bot has been up for " + Main.MANAGER.getStats(event.getBot().getBotId()).getUptime());
 	}
 }
