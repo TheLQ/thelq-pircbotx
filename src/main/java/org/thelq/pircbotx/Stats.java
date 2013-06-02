28534ddf5dfc0fe8458ba8612cfecffb9aaaf161
Leon Blakey <lord.quackstar@gmail.com>
2013-06-02 03:34:10 -0400

    New methods to get users and channels easily

--- src/main/java/org/thelq/pircbotx/Stats.java
+++ src/main/java/org/thelq/pircbotx/Stats.java
@@ -22,6 +22,9 @@
  */
 package org.thelq.pircbotx;
 
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Iterables;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -57,11 +60,32 @@ public class Stats {
 
 	@Data
 	public static class HistoryEntry {
+		protected static final Joiner SPACE_JOINER = Joiner.on(", ");
+		protected static final Function FUNCTION_CHANNEL_NAME = new Function<Channel, String>() {
+			@Override
+			public String apply(Channel input) {
+				return input.getName();
+			}
+		};
+		protected static final Function FUNCTION_USER_NICK = new Function<User, String>() {
+			@Override
+			public String apply(User input) {
+				return input.getNick();
+			}
+		};
 		protected final HistoryType type;
 		protected final String timestamp;
 		protected final List<Channel> channels;
 		protected final List<User> users;
 		protected final String message;
+		
+		public String getChannelNames() {
+			return SPACE_JOINER.join(Iterables.transform(channels, FUNCTION_CHANNEL_NAME));
+		}
+		
+		public String getUserNames() {
+			return SPACE_JOINER.join(Iterables.transform(users, FUNCTION_USER_NICK));
+		}
 	}
 
 	public static enum HistoryType {
