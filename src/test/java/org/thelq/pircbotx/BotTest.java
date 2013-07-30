3458cebc750798d44667b86ac65fe493042ee6af
Leon Blakey <lord.quackstar@gmail.com>
2013-07-30 16:12:42 -0400

    Use correct annotation

--- src/test/java/org/thelq/pircbotx/BotTest.java
+++ src/test/java/org/thelq/pircbotx/BotTest.java
@@ -13,8 +13,8 @@ import java.net.URL;
 import java.util.Enumeration;
 import java.util.Properties;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
 import static org.testng.Assert.*;
+import org.testng.annotations.Test;
 
 /**
  *
