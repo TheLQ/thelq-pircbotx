53f2fb2e8fe3c018f2d05ca57919ee57fd5d1c97
Leon Blakey <lord.quackstar@gmail.com>
2013-07-30 16:00:09 -0400

    Added properties test

---
+++ src/test/java/org/thelq/pircbotx/BotTest.java
@@ -0,0 +1,46 @@
+/**
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.thelq.pircbotx;
+
+import com.google.common.io.Resources;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Properties;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import static org.testng.Assert.*;
+
+/**
+ *
+ * @author Leon
+ */
+@Slf4j
+public class BotTest {
+	@Test
+	public void propertiesTest() throws IOException, URISyntaxException {
+		getResources("");
+		getResources(".");
+		getResources("/");
+		Properties properties = new Properties();
+		properties.load(new FileInputStream(new File(Resources.getResource("pircbotx.properties").toURI())));
+		assertTrue(properties.size() > 3, "Not enough properties: " + properties.size());
+	}
+
+	protected static void getResources(String path) {
+		try {
+			log.info(path);
+			for (Enumeration<URL> resources = BotTest.class.getClassLoader().getResources(path); resources.hasMoreElements();) {
+				URL curResource = resources.nextElement();
+				log.info(" - " + curResource);
+			}
+		} catch (Exception e) {
+			log.warn("Cannot get resources for " + path, e);
+		}
+	}
+}
