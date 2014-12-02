57bb10174c29fe1c344f484042ca4ffce52aa52e
Leon Blakey <lord.quackstar@gmail.com>
2014-12-02 10:43:19 -0500

    Use mutch cleaner addListener in configBuilder

--- src/main/java/org/thelq/pircbotx/Main.java
+++ src/main/java/org/thelq/pircbotx/Main.java
@@ -3,18 +3,18 @@
  *
  * This file is part of TheLQ-PircBotX.
  *
- * TheLQ-PircBotX is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * TheLQ-PircBotX is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
  *
- * TheLQ-PircBotX is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * TheLQ-PircBotX is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with TheLQ-PircBotX. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * TheLQ-PircBotX. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.thelq.pircbotx;
 
@@ -59,27 +59,27 @@ public class Main {
 		//Initial configuration
 		Configuration.Builder templateConfig = new Configuration.Builder()
 				.setLogin("LQ")
-				.setAutoNickChange(true);
+				.setAutoNickChange(true)
+				.addListener(new HelpCommand())
+				.addListener(new IdentifiedCommand())
+				.addListener(new UptimeCommand())
+				.addListener(new LevelsListCommand())
+				.addListener(new MyLevelsCommand())
+				.addListener(new CountdownCommand())
+				.addListener(new NewYearsCommand())
+				.addListener(new StatsCommand())
+				.addListener(new NickUpdateListener());
 		if (PRODUCTION)
 			templateConfig.setName("TheLQ-PircBotX");
 		else
 			templateConfig.setName("TheLQ-BotTest");
-		templateConfig.getListenerManager().addListener(new HelpCommand());
-		templateConfig.getListenerManager().addListener(new IdentifiedCommand());
-		templateConfig.getListenerManager().addListener(new UptimeCommand());
-		templateConfig.getListenerManager().addListener(new LevelsListCommand());
-		templateConfig.getListenerManager().addListener(new MyLevelsCommand());
-		templateConfig.getListenerManager().addListener(new CountdownCommand());
-		templateConfig.getListenerManager().addListener(new NewYearsCommand());
-		templateConfig.getListenerManager().addListener(new StatsCommand());
-		templateConfig.getListenerManager().addListener(new NickUpdateListener());
 
 		//Load properties
 		String filename = "thelq-pircbotx.toml";
 		File file = new File(filename);
-		if(!file.exists())
+		if (!file.exists())
 			file = new File("src/main/resources/" + filename);
-		if(!file.exists())
+		if (!file.exists())
 			throw new RuntimeException("Cannot find file " + filename);
 		Toml properties = new Toml().parse(file);
 
@@ -126,7 +126,7 @@ public class Main {
 
 		server.start();
 	}
-	
+
 	@EqualsAndHashCode
 	@ToString
 	@RequiredArgsConstructor
